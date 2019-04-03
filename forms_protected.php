0bb37bc45f5d35f4f48a969167f8eea5a0e538e9
Texas Network Group <rea@texasnetworkgroup.com>
2019-04-03 15:03:19 -0500

    Changed site to php from html so I can change footer to an includes for easy modify

--- forms_protected.php
+++ forms_protected.php
@@ -8,7 +8,7 @@ include "includes/password_protect.php";
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<title>Download Forms and Applications</title>
+<title>New Braunfels Noon Lions Protected Documents</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="description" content="" />
 <meta name="author" content="http://webthemez.com" />
@@ -30,19 +30,6 @@ include "includes/password_protect.php";
 	<!-- start header -->
 	<header>
 
-		<!--<section class="contactInfo">-->
-		<!--<div class="container">-->
-		<!--<div class="col-md-12">-->
-		<!--<div class="contact-area">-->
-		<!--<ul>-->
-		<!--<li><i class="fa fa-phone-square"></i>812-123-456</li>-->
-		<!--<li><i class="fa fa-envelope-o"></i>info@Delight.com</li>-->
-		<!--</ul>-->
-		<!--</div>-->
-		<!--</div>-->
-		<!--</div>-->
-		<!--</section>-->
-
 		<div class="navbar navbar-default navbar-static-top">
 			<div class="container">
 				<div class="navbar-header">
@@ -51,16 +38,16 @@ include "includes/password_protect.php";
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="index.html"><img src="img/logoLongA.png" alt="New Braunfels Lions"/></a>
+					<a class="navbar-brand" href="index.php"><img src="img/logoLongA.png" alt="New Braunfels Lions"/></a>
 				</div>
 				<div class="navbar-collapse collapse ">
 					<ul class="nav navbar-nav">
-						<li><a href="index.html">Home</a></li>
-						<li><a href="about.html">About Us</a></li>
-						<li><a href="newsletters.html">Newsletters</a></li>
-						<li class="active"><a href="forms.html">Forms/Links</a></li>
-						<li><a href="gallery.html">Gallery</a></li>
-						<li><a href="contact.html">Contact</a></li>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="about.php">About Us</a></li>
+						<li><a href="newsletters.php">Newsletters</a></li>
+						<li class="active"><a href="forms.php">Forms/Links</a></li>
+						<li><a href="gallery.php">Gallery</a></li>
+						<li><a href="contact.php">Contact</a></li>
 					</ul>
 				</div>
 			</div>
@@ -70,7 +57,7 @@ include "includes/password_protect.php";
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-12">
-				<h2 class="pageTitle">Info, Forms, Applications</h2>
+				<h2 class="pageTitle">New Braunfels Noon Lions Protected Documents</h2>
 			</div>
 		</div>
 	</div>
@@ -79,151 +66,52 @@ include "includes/password_protect.php";
 		<div class="container content">
 			<div class="row">
 				<div class="col-md-12">
-					<h3><a href="formDownloads/bylaws.pdf" target="_blank">By-Laws</a></h3>
-					<p>The International Association of Lions Clubs constitution and By-Laws detail the organization and operations of all clubs. Each member is
-					encouraged to read through these documents.</p>
+					<h3><a href="secureDocs/lionsdirectory.pdf" target="_blank">Directory - Click Here</a></h3>
+					<p>New Braunfels Noon Lions Directory - This directory will be updated on a regular basis</p>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-md-12">
-					<h3><a href="formDownloads/memberApp.pdf" target="_blank">Member Application</a></h3>
-					<p>Lions meet the needs of local communities and the world. The 1.4 million members of our volunteer organization in 206 countries and geographic
-						areas are different in many ways, but share a core belief – community is what we make it.</p>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-12">
-					<h3><a href="formDownloads/waystoGive.pdf" target="_blank">Ways to Give</a></h3>
-					<p>Lions around the world are united in our spirit of bringing healing and hope to those who are most in need. Through Lions Clubs International
-						Foundation (LCIF), 1.4 million Lions in 210 countries are able to turn compassion into action. As the official charitable organization of Lions
-						Clubs International, LCIF has awarded 12,701 grants totaling more than US$950 million since its founding in 1968. The generosity of Lions has
-						provided most of this revenue. But, there are still many more people who are counting on Lions’ help.</p>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-12">
-					<h3><a href="formDownloads/singleDonor.pdf" target="_blank">Single Donor Form</a></h3>
-					<p>Each donation is important to fulfilling LCIF’s mission. When you make a donation to LCIF, you can be sure that your contribution is making a
-						difference in the lives of people in need.</p>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-12">
-					<h3><a href="formDownloads/Eyeglass_Recycling_Lions_Clubs.pdf" target="_blank">Eyeglass Recycling Information</a></h3>
-					<p>In just about any home, one can  nd a pair of eyeglasses that are no longer being used. That same pair of eyeglasses can change another person's
-						life.</p>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-12">
-					<h3><u>LINKS</u></h3>
-					<p><a href="http://lsleb.org/" target="_blank">Lion's Eye Bank</a></p>
-					<p><a href="https://www.donatelifetexas.org/" target="_blank">Donate Life Texas</a></p><br/>
-					<p><b>Texas Lion's Camp</b><br/>
-						<a href="http://www.lionscamp.com/default.html" target="_blank">Texas Lion's Camp Information</a><br/>
-						<a href="http://www.lionscamp.com/DownloadCenter.html" target="_blank">Download Applications</a>
-					</p>
-				</div>
-			</div>
-		</div>
-
-
-		<!--<div class="container">	 -->
-			<!--&lt;!&ndash; end divider &ndash;&gt;-->
-			<!--<div class="row">-->
-				<!--<div class="col-lg-12">-->
-					<!--<h4>Our best plans</h4>-->
-				<!--</div>-->
-				<!--<div class="col-md-3">-->
-					<!--<div class="pricing-box-item">-->
-						<!--<div class="pricing-heading">-->
-							<!--<h3><strong>Basic</strong></h3>-->
-						<!--</div>-->
-						<!--<div class="pricing-terms">-->
-							<!--<h6>&#36;15.00 / Year</h6>-->
-						<!--</div>-->
-						<!--<div class="pricing-container">-->
-							<!--<ul>-->
-								<!--<li><i class="icon-ok"></i> Responsive Design</li>-->
-								<!--<li><i class="icon-ok"></i> Bootstrap Design</li>-->
-								<!--<li><i class="icon-ok"></i> Unlimited Support</li>-->
-								<!--<li><i class="icon-ok"></i> Free Trial version</li>-->
-								<!--<li><i class="icon-ok"></i> HTML5 CSS3 jQuery</li>-->
-							<!--</ul>-->
-						<!--</div>-->
-						<!--<div class="pricing-action">-->
-							<!--<a href="#" class="btn btn-medium btn-theme"><i class="icon-bolt"></i> Get Now</a>-->
-						<!--</div>-->
-					<!--</div>-->
-				<!--</div>-->
-				<!--<div class="col-md-3">-->
-					<!--<div class="pricing-box-item">-->
-						<!--<div class="pricing-heading">-->
-							<!--<h3><strong>Standard</strong></h3>-->
-						<!--</div>-->
-						<!--<div class="pricing-terms">-->
-							<!--<h6>&#36;20.00 / Year</h6>-->
-						<!--</div>-->
-						<!--<div class="pricing-container">-->
-							<!--<ul>-->
-								<!--<li><i class="icon-ok"></i> Responsive Design</li>-->
-								<!--<li><i class="icon-ok"></i> Bootstrap Design</li>-->
-								<!--<li><i class="icon-ok"></i> Unlimited Support</li>-->
-								<!--<li><i class="icon-ok"></i> Free Trial version</li>-->
-								<!--<li><i class="icon-ok"></i> HTML5 CSS3 jQuery</li>-->
-							<!--</ul>-->
-						<!--</div>-->
-						<!--<div class="pricing-action">-->
-							<!--<a href="#" class="btn btn-medium btn-theme"><i class="icon-bolt"></i> Get Now</a>-->
-						<!--</div>-->
-					<!--</div>-->
-				<!--</div>-->
-				<!--<div class="col-md-3">-->
-					<!--<div class="pricing-box-item activeItem">-->
-						<!--<div class="pricing-heading">-->
-							<!--<h3><strong>Advanced</strong></h3>-->
-						<!--</div>-->
-						<!--<div class="pricing-terms">-->
-							<!--<h6>&#36;15.00 / Year</h6>-->
-						<!--</div>-->
-						<!--<div class="pricing-container">-->
-							<!--<ul>-->
-								<!--<li><i class="icon-ok"></i> Responsive Design</li>-->
-								<!--<li><i class="icon-ok"></i> Bootstrap Design</li>-->
-								<!--<li><i class="icon-ok"></i> Unlimited Support</li>-->
-								<!--<li><i class="icon-ok"></i> Free Trial version</li>-->
-								<!--<li><i class="icon-ok"></i> HTML5 CSS3 jQuery</li>-->
-							<!--</ul>-->
-						<!--</div>-->
-						<!--<div class="pricing-action">-->
-							<!--<a href="#" class="btn btn-medium btn-theme"><i class="icon-bolt"></i> Get Now</a>-->
-						<!--</div>-->
-					<!--</div>-->
-				<!--</div>-->
-				<!--<div class="col-md-3">-->
-					<!--<div class="pricing-box-item">-->
-						<!--<div class="pricing-heading">-->
-							<!--<h3><strong>Mighty</strong></h3>-->
-						<!--</div>-->
-						<!--<div class="pricing-terms">-->
-							<!--<h6>&#36;15.00 / Year</h6>-->
-						<!--</div>-->
-						<!--<div class="pricing-container">-->
-							<!--<ul>-->
-								<!--<li><i class="icon-ok"></i> Responsive Design</li>-->
-								<!--<li><i class="icon-ok"></i> Bootstrap Design</li>-->
-								<!--<li><i class="icon-ok"></i> Unlimited Support</li>-->
-								<!--<li><i class="icon-ok"></i> Free Trial version</li>-->
-								<!--<li><i class="icon-ok"></i> HTML5 CSS3 jQuery</li>-->
-							<!--</ul>-->
-						<!--</div>-->
-						<!--<div class="pricing-action">-->
-							<!--<a href="#" class="btn btn-medium btn-theme"><i class="icon-bolt"></i> Get Now</a>-->
-						<!--</div>-->
-					<!--</div>-->
-				<!--</div>-->
-			<!--</div>-->
-		<!--</div>-->
+<!--			<div class="row">-->
+<!--				<div class="col-md-12">-->
+<!--					<h3><a href="formDownloads/memberApp.pdf" target="_blank">Member Application</a></h3>-->
+<!--					<p>Lions meet the needs of local communities and the world. The 1.4 million members of our volunteer organization in 206 countries and geographic-->
+<!--						areas are different in many ways, but share a core belief – community is what we make it.</p>-->
+<!--				</div>-->
+<!--			</div>-->
+<!--			<div class="row">-->
+<!--				<div class="col-md-12">-->
+<!--					<h3><a href="formDownloads/waystoGive.pdf" target="_blank">Ways to Give</a></h3>-->
+<!--					<p>Lions around the world are united in our spirit of bringing healing and hope to those who are most in need. Through Lions Clubs International-->
+<!--						Foundation (LCIF), 1.4 million Lions in 210 countries are able to turn compassion into action. As the official charitable organization of Lions-->
+<!--						Clubs International, LCIF has awarded 12,701 grants totaling more than US$950 million since its founding in 1968. The generosity of Lions has-->
+<!--						provided most of this revenue. But, there are still many more people who are counting on Lions’ help.</p>-->
+<!--				</div>-->
+<!--			</div>-->
+<!--			<div class="row">-->
+<!--				<div class="col-md-12">-->
+<!--					<h3><a href="formDownloads/singleDonor.pdf" target="_blank">Single Donor Form</a></h3>-->
+<!--					<p>Each donation is important to fulfilling LCIF’s mission. When you make a donation to LCIF, you can be sure that your contribution is making a-->
+<!--						difference in the lives of people in need.</p>-->
+<!--				</div>-->
+<!--			</div>-->
+<!--			<div class="row">-->
+<!--				<div class="col-md-12">-->
+<!--					<h3><a href="formDownloads/Eyeglass_Recycling_Lions_Clubs.pdf" target="_blank">Eyeglass Recycling Information</a></h3>-->
+<!--					<p>In just about any home, one can  nd a pair of eyeglasses that are no longer being used. That same pair of eyeglasses can change another person's-->
+<!--						life.</p>-->
+<!--				</div>-->
+<!--			</div>-->
+<!--			<div class="row">-->
+<!--				<div class="col-md-12">-->
+<!--					<h3><u>LINKS</u></h3>-->
+<!--					<p><a href="http://lsleb.org/" target="_blank">Lion's Eye Bank</a></p>-->
+<!--					<p><a href="https://www.donatelifetexas.org/" target="_blank">Donate Life Texas</a></p><br/>-->
+<!--					<p><b>Texas Lion's Camp</b><br/>-->
+<!--						<a href="http://www.lionscamp.com/default.html" target="_blank">Texas Lion's Camp Information</a><br/>-->
+<!--						<a href="http://www.lionscamp.com/DownloadCenter.html" target="_blank">Download Applications</a>-->
+<!--					</p>-->
+<!--				</div>-->
+<!--			</div>-->
+<!--		</div>-->
 
 	</section>
 	<footer>
@@ -234,12 +122,12 @@ include "includes/password_protect.php";
 					<div class="widget">
 						<h5 class="widgetheading">Quick Links</h5>
 						<ul class="link-list">
-							<li><a href="index.html">Home</a></li>
-							<li><a href="about.html">About Us</a></li>
-							<li><a href="newsletters.html">Newsletter</a></li>
-							<li><a href="forms.html">Forms/Links</a></li>
-							<li><a href="gallery.html">Gallery</a></li>
-							<li><a href="contact.html">Contact Us</a></li>
+							<li><a href="index.php">Home</a></li>
+							<li><a href="about.php">About Us</a></li>
+							<li><a href="newsletters.php">Newsletter</a></li>
+							<li><a href="forms.php">Forms/Links</a></li>
+							<li><a href="gallery.php">Gallery</a></li>
+							<li><a href="contact.php">Contact Us</a></li>
 							<li><a href="http://newbraunfelslions.com/files" target="_blank">Files</a></li>
 						</ul>
 					</div>
