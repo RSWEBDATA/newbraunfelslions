018adb21656cc6dc6f1ea92b999200ed10b30421
Texas Network Group <rea@texasnetworkgroup.com>
2019-04-17 22:02:24 -0500

    Made a few updates of pictures and special events happening.

--- forms.php
+++ forms.php
@@ -231,78 +231,8 @@
 		<!--</div>-->
 
 	</section>
-	<footer>
-		<div class="container">
-			<div class="row">
-
-				<div class="col-md-3">
-					<div class="widget">
-						<h5 class="widgetheading">Quick Links</h5>
-						<ul class="link-list">
-							<li><a href="index.php">Home</a></li>
-							<li><a href="about.php">About Us</a></li>
-							<li><a href="newsletters.php">Newsletter</a></li>
-							<li><a href="forms.php">Forms/Links</a></li>
-							<li><a href="gallery.php">Gallery</a></li>
-							<li><a href="contact.php">Contact Us</a></li>
-							<li><a href="http://newbraunfelslions.com/files" target="_blank">Files</a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="widget">
-						<h5 class="widgetheading">Contact Info</h5>
-						<address>
-							<strong>New Braunfels Noon Lions Club</strong><br>
-							PO Box 310194<br>
-							New Braunfels, TX 78131</address>
-						<p>
-							<i class="icon-phone"></i> 512-426-4292 <br>
-							<i class="icon-envelope-alt"></i> info@newbraunfelslions.com
-						</p>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="widget">
-						<h5 class="widgetheading">We Meet At</h5>
-						<address>
-							<strong>Seekatz Opera House</strong><br>
-							265 W. San Antonio St. <br>
-							New Braunfels, TX 78130</address>
-						<p>Every 1st & 3rd Thursday at 12:00 Noon</p>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="widget">
-						<h5 class="widgetheading">&nbsp;</h5>
-						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3465.6413022558354!2d-98.12797374941393!3d29.70117754157331!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x865cbd677db9a73d%3A0xd8be44b2142e0fca!2sSeekatz+Opera+House!5e0!3m2!1sen!2sus!4v1529946694372" width="95%" frameborder="0" style="border:0; margin: 0 auto 0 auto;" allowfullscreen></iframe>
-					</div>
-				</div>
-			</div>
-		</div>
-	<div id="sub-footer">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-6">
-					<div class="copyright">
-						<p>
-							<span>&copy; Delight 2015 All right reserved. By </span><a href="http://webthemez.com" Delight="_blank">WebThemez</a>
-						</p>
-					</div>
-				</div>
-				<div class="col-lg-6">
-					<ul class="social-network">
-						<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-						<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-						<li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
-						<li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
-						<li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus"></i></a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
-	</footer>
+    <!--include footer file below-->
+    <?php include "includes/footer.php";?>
 </div>
 <a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
 <!-- javascript
--- index.php
+++ index.php
@@ -26,18 +26,7 @@
 	<!-- start header -->
 	<header>
 
-	<!--<section class="contactInfo">-->
-		<!--<div class="container">-->
-				<!--<div class="col-md-12">-->
-						<!--<div class="contact-area">-->
-							<!--<ul>-->
-								<!--<li><i class="fa fa-phone-square"></i>812-123-456</li>-->
-								<!--<li><i class="fa fa-envelope-o"></i>info@Delight.com</li>-->
-							<!--</ul>-->
-						<!--</div>-->
-				<!--</div>-->
-		<!--</div>-->
-	<!--</section>-->
+
 
         <div class="navbar navbar-default navbar-static-top">
             <div class="container">
@@ -69,54 +58,73 @@
         <div id="main-slider" class="flexslider">
             <ul class="slides">
 				<li>
-					<img src="img/slides/HeritageSocietyDonation.jpg" alt="Heritage Society Donation 2019" height="900" />
+					<img src="img/slides/howardw.png" alt="Heritage Society Donation 2019" height="900" />
 					<div class="flex-caption">
-						<h3>New Braunfels Heritage Society</h3>
-						<p>With a little help from the NB Lions</p>
+						<h3>Our Speaker for today is...</h3>
+						<p>Join us for lunch to see who it is</p>
 					</div>
 				</li>
               <li>
-                <img src="img/slides/KarenCarifee.jpg" alt="New Members" height="900"/>
+                <img src="img/slides/BOTB2019.png" alt="New Members" height="900"/>
                 <div class="flex-caption">
-                    <h3>Welcome to New Members</h3>
-					<p>We are glad to have you join the family</p>
+                    <h3>LET THE BATTLE BEGIN!</h3>
+					<p>We support our LEO family with special functions</p>
                 </div>
               </li>
               <li>
-                <img src="img/slides/ChristianMinistries.jpg" alt="" height="900" />
+                <img src="img/slides/wurstfest-small.png" alt="" height="900" />
                 <div class="flex-caption">
-                    <h3>Christian Ministries</h3>
-					<p>We are here to support you</p>
+                    <h3>Did we mention Wurstfest</h3>
+					<p>A 10 day salute to sausage, beer and fun</p>
                 </div>
               </li>
               <li>
-                <img src="img/slides/lci-1480.png" alt="" height="900" />
+                <img src="img/slides/jackandnewmembers.png" alt="" height="900" />
                 <div class="flex-caption">
-                    <h3>WE GOT THE WHOLE WORLD</h3>
-					<p>In Our Hands</p>
+                    <h3>Jack welcomes some new members</h3>
+					<p>Great Job!</p>
                 </div>
               </li>
             </ul>
         </div>
 	<!-- end slider -->
  
-	</section> 
+	</section>
+
+    <section class="alert-warning">
+        <div class="container">
+            <div class="col-md-6">
+                <div class="container">
+                    <h1 style="text-align: center;">Battle of the Bands at Freiheit Country Store</h1>
+                    <h1 style="text-align: center;"> <a href="img/LEOS/BOTB2019.pdf" data-toggle="tooltip" title="Come join the fun on April 25, 2019">
+                            More Information Here</a></h1>
+                </div>
+            </div>
+        </div>
+    </section>
 		<section id="content">
 	
 	<div class="container">
 			<div class="row">
-				<div class="col-lg-6 col-md-6 col-sm-6">
-                 <img src="img/lionsongleader.jpg" alt="New Braunfels Lions all in song" />
-				</div>
-				<div class="col-lg-6 col-md-6 col-sm-6">
+
+                <div class="col-lg-5 col-md-5 col-sm-5">
+                    <img src="img/Joker.jpg" alt="I got the joker" />
+                </div>
+
+                <div class="col-lg-7 col-md-7 col-sm-7">
+                    <a href="https://www.signupgenius.com/go/10C0D4BA4A62BA4F85-white1"><img src="img/whiteCaneDay.png"></a>
+                </div>
+
+				<div class="col-lg-10 col-md-10 col-sm-10">
                     <div>
-					    <h2>New Braunfels Noon Lions Club</h2>
-    					<p>Lions are changing the world one community at a time, by addressing needs at home and around the globe. We are 1.4 million men and women who believe that kindness matters. And when we work
-							together, we can achieve bigger goals. Our mission is to create and foster a spirit of understanding among all people for humanitarian needs by providing voluntary services through community
-							involvement and international cooperation.</p>
-						<p>We encourage service minded people to serve their community without personal financial reward, to encourage effiency and to promote high ethical standards in commerce, industries, professions,
-							public works and private endeavors, to promote the principles of good government and good citizenship, and to take an active interest in the civic, cultural, social and moral welfare of the
-							community . . . <span><i><a href="about.php"><br>read more.</a></i></span></p>
+					    <h2>Braunfels Noon Lions Club</h2><br>
+                        <h3>Ask me about the joker</h3>
+                            <p>Lions are changing the world one community at a time, by addressing needs at home and around the globe. We are 1.4 million men and women who believe that kindness matters. And when we work
+                                together, we can achieve bigger goals. Our mission is to create and foster a spirit of understanding among all people for humanitarian needs by providing voluntary services through community
+                                involvement and international cooperation.</p>
+                            <p>We encourage service minded people to serve their community without personal financial reward, to encourage effiency and to promote high ethical standards in commerce, industries, professions,
+                                public works and private endeavors, to promote the principles of good government and good citizenship, and to take an active interest in the civic, cultural, social and moral welfare of the
+                                community . . . <span><i><a href="about.php"><br>read more.</a></i></span></p>
 						<br/>
 						<p><b>Join Us at</b>
 						<address>
@@ -125,38 +133,59 @@
 							New Braunfels, TX 78130</address>
 						<p>Every 1st & 3rd Thursday at 12:00 Noon</p>
 						</p>
-    					<!--<div class="row">-->
-    						<!--<div class="col-lg-4 col-md-4 col-sm-4">-->
-    							<!--<ul class="list list-3">-->
-    								<!--<li>Designing</li>-->
-                                    <!--<li>Development</li>-->
-                                    <!--<li>Web Services</li>-->
-                                    <!--<li>Hosting</li>-->
-    							<!--</ul>-->
-                            <!--</div>-->
-                            <!--<div class="col-lg-4 col-md-4 col-sm-4">-->
-    							<!--<ul class="list list-3">-->
-    								<!--<li>Copy writing</li>-->
-                                    <!--<li>On-line Shopping</li>-->
-                                    <!--<li>SAP</li>-->
-                                    <!--<li>Testing</li>-->
-    							<!--</ul>-->
-                            <!--</div>-->
-                            <!--<div class="col-lg-4 col-md-4 col-sm-4">-->
-    							<!--<ul class="list list-3">-->
-    								<!--<li>Mobile</li>-->
-                                    <!--<li>Database</li>-->
-                                    <!--<li>User Experience</li>-->
-                                    <!--<li>Product</li>-->
-    							<!--</ul>-->
-    						<!--</div>-->
-    					<!--</div>-->
+
+
+
+<!--    					<div class="row">-->
+<!--    						<div class="col-lg-4 col-md-4 col-sm-4">-->
+<!--    							<ul class="list list-3">-->
+<!--    								<li>Designing</li>-->
+<!--                                    <li>Development</li>-->
+<!--                                    <li>Web Services</li>-->
+<!--                                    <li>Hosting</li>-->
+<!--    							</ul>-->
+<!--                            </div>-->
+<!--                            <div class="col-lg-4 col-md-4 col-sm-4">-->
+<!--    							<ul class="list list-3">-->
+<!--    								<li>Copy writing</li>-->
+<!--                                    <li>On-line Shopping</li>-->
+<!--                                    <li>SAP</li>-->
+<!--                                    <li>Testing</li>-->
+<!--    							</ul>-->
+<!--                            </div>-->
+<!--                            <div class="col-lg-4 col-md-4 col-sm-4">-->
+<!--    							<ul class="list list-3">-->
+<!--    								<li>Mobile</li>-->
+<!--                                    <li>Database</li>-->
+<!--                                    <li>User Experience</li>-->
+<!--                                    <li>Product</li>-->
+<!--    							</ul>-->
+<!--    						</div>-->
+<!--    					</div>-->
+
+
+
                     </div>
 				</div>
 			</div>
 		</div>
 	 
 	</section>
+
+    <section class="contactInfo">
+        <div class="container">
+            <div class="col-md-6">
+                <div class="container">
+
+                    <h1>Award Ceremonies - Celebrating with our LEO Families
+                        <ul>
+                            <li><p><h2>New Braunfels HS April 29 at 6:30pm</h2></p></li>
+                            <li><p><h2>Canyon HS - May 23 at 6:00pm</h2></p></li>
+                        </ul></h1>
+                </div>
+            </div>
+        </div>
+    </section>
 	
 	
 	<!--<section id="service">-->
@@ -233,8 +262,8 @@
 			</div>
 			<div class="col-md-6" align="center" style="color: #FFFFFF;">
 				<div class="col-md-6">
-					<h2 style="color: #FFFFFF;">Pay On-Line</h2>
-					<p style="color: #FFFFFF;">We now offer on-line payments through PayPal. <br/>Just click on the button.</p>
+					<h2 style="color: #FFFFFF;">Donate On-Line</h2>
+					<p style="color: #FFFFFF;">We now offer on-line Donations through PayPal</p>
 				</div>
 				<div class="col-md-6" align="center">
 					<div class="appLogosIndex"><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=RTFB2L3ZKYMNW" target="_blank">
@@ -285,79 +314,8 @@
             <!--</div>-->
         </div>
     </div>
-</div>
-	<footer>
-	<div class="container">
-		<div class="row">
-
-			<div class="col-md-3">
-				<div class="widget">
-					<h5 class="widgetheading">Quick Links</h5>
-					<ul class="link-list">
-						<li><a href="index.php">Home</a></li>
-						<li><a href="about.php">About Us</a></li>
-						<li><a href="newsletters.php">Newsletter</a></li>
-						<li><a href="forms.php">Forms/Links</a></li>
-						<li><a href="gallery.php">Gallery</a></li>
-						<li><a href="contact.php">Contact Us</a></li>
-						<li><a href="forms_protected.php" target="_blank">Files</a></li>
-					</ul>
-				</div>
-			</div>
-			<div class="col-md-3">
-				<div class="widget">
-					<h5 class="widgetheading">Contact Info</h5>
-					<address>
-					<strong>New Braunfels Noon Lions Club</strong><br>
-					PO Box 310194<br>
-					 New Braunfels, TX 78131</address>
-					<p>
-						<i class="icon-phone"></i> 512-426-4292 <br>
-						<i class="icon-envelope-alt"></i> info@newbraunfelslions.com
-					</p>
-				</div>
-			</div>
-			<div class="col-md-3">
-				<div class="widget">
-					<h5 class="widgetheading">We Meet At Version control</h5>
-					<address>
-						<strong>Seekatz Opera House</strong><br>
-						265 W. San Antonio St. <br>
-						New Braunfels, TX 78130</address>
-					<p>Every 1st & 3rd Thursday at 12:00 Noon</p>
-				</div>
-			</div>
-			<div class="col-md-3">
-				<div class="widget">
-					<h5 class="widgetheading">&nbsp;</h5>
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3465.6413022558354!2d-98.12797374941393!3d29.70117754157331!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x865cbd677db9a73d%3A0xd8be44b2142e0fca!2sSeekatz+Opera+House!5e0!3m2!1sen!2sus!4v1529946694372" width="95%" frameborder="0" style="border:0; margin: 0 auto 0 auto;" allowfullscreen></iframe>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div id="sub-footer">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-6">
-					<div class="copyright">
-						<p>
-							<span>&copy; Delight 2015 All right reserved. By </span><a href="http://webthemez.com" Delight="_blank">WebThemez</a>
-						</p>
-					</div>
-				</div>
-				<div class="col-lg-6">
-					<ul class="social-network">
-						<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-						<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-						<li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
-						<li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
-						<li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus"></i></a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
-	</footer>
+<!--include footer file below-->
+<?php include "includes/footer.php";?>
 </div>
 <a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
 <!-- javascript
